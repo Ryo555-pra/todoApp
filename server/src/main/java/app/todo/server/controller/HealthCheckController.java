0a03ecf2886b02ec1b59fdaf74664ddc7f17ec7f
Ryo555-pra <nakaryou1214@gmail.com>
2025-08-18 01:54:32 +0900

    pass bootRun & make healthcheack

---
+++ server/src/main/java/app/todo/server/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package app.todo.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
